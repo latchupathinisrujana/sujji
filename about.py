cebd184d64d3c844245f52a62fffe0a50cf04849
latchupathinisrujana <latchupathinisrujana@gmail.com>
2024-02-07 21:29:30 +0530

    mm

---
+++ about.py
@@ -0,0 +1,7 @@
+print("To add two numbers.")
+print("Enter the first number: ", end="")
+num1 = int(input())
+print("Enter second number: ", end="")
+num2 = int(input())
+sum = num1 + num2
+print("The sum of " , num1 , " and " ,num2, " is " , sum, "." ,sep="")
